2beb8edf327e42aa52fcbf705a7230b6cccec0b7
LvYuanWang <1175430934@qq.com>
2025-01-05 21:59:45 +0800

    feat: vue3练习实现商品购物车

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,47 @@
 <template>
-  <h1 style="text-align: center;">表单处理</h1>
-  <div class="container">
-    <hr class="hr" />
-    <div>
-      <h1>表单元素的数据绑定</h1>
-      <h2>文本域</h2>
-      <textarea cols="30" rows="5" v-model="textContent"></textarea>
-      <p>你当前输入的内容为: {{ textContent }}</p>
-      <hr>
-      <h2>复选框</h2>
-      <input type="checkbox" v-model="isChecked" />
-      <button @click="isChecked = !isChecked">{{ !isChecked }}</button>
-      <hr>
-      <h2>自定义真假值</h2>
-      <input type="checkbox" true-value="yes" false-value="no" v-model="checkedValue" />
-      <button
-        @click="checkedValue === 'yes' ? (checkedValue = 'no') : (checkedValue = 'yes')">{{ checkedValue }}</button>
-      <hr>
-      <h2>多复选框</h2>
-      <div v-for="(item, index) in checkedArrData" :key="index">
-        <label for="item.id">{{ item.title }}</label>
-        <input type="checkbox" :id="item.id" :value="item.value" v-model="hobby" />
+  <div id="app">
+    <table>
+      <tr>
+        <th>
+          <input type="checkbox" v-model="chooseAllShopping" />
+          全选
+        </th>
+        <th>商品</th>
+        <th>单价(元)</th>
+        <th>数量</th>
+        <th>小计(元)</th>
+        <th>操作</th>
+      </tr>
+      <tr v-for="shopping in shoppingData" :key="shopping.id">
+        <td>
+          <input type="checkbox" v-model="shopping.checked" />
+        </td>
+        <td>{{ shopping.goods_name }}</td>
+        <td>{{ shopping.goods_price }}</td>
+        <td>
+          <button @click="shopping.goods_num !== 0 && shopping.goods_num--">-</button>
+          <input type="text" v-model="shopping.goods_num" class="priceNum" />
+          <button @click="shopping.goods_num++">+</button>
+        </td>
+        <td>{{ shopping.goods_price * shopping.goods_num }}</td>
+        <td>
+          <button class="delete" @click="deleteShopping(shopping.id)">删除</button>
+        </td>
+      </tr>
+    </table>
+    <div class="footer">
+      <div class="footer-left">
+        <div class="checkboxBtn">
+          <input type="checkbox" v-model="chooseAllShopping" />
+          全选
+        </div>
+        <div class="deleteChoose" style="cursor: pointer;" @click="deleteChooseShopping">删除选中的商品</div>
+        <div>共{{ shoppingData.length }}件商品, 已选择{{ shoppingData.filter(item => item.checked).length }}件</div>
       </div>
-      <p>{{ message || "请选择爱好!!" }}</p>
-      <hr>
-      <h2>单选框</h2>
-      <input type="radio" id="male" value="男" v-model="gender">
-      <label for="male">男</label>
-      <input type="radio" id="female" value="女" v-model="gender">
-      <label for="female">女</label>
-      <input type="radio" id="hiddenVal" value="保密" v-model="gender">
-      <label for="hiddenVal">保密</label>
-      <hr>
-      <h2>下拉列表</h2>
-      <select v-model="selectValue">
-        <option value="" disabled>请选择</option>
-        <option v-for="(item, index) in selectArrData" :key="index" :value="item.key">{{ item.value }}</option>
-      </select>
-      <p>你选择的英雄类型是: {{ selectValue }}</p>
-      <hr>
-      <h2>多下拉列表</h2>
-      <select v-model="selectValues" multiple>
-        <option value="" disabled>请选择</option>
-        <option v-for="(item, index) in selectArrData" :key="index" :value="item.key">{{ item.value }}</option>
-      </select>
-      <p>你喜欢玩的英雄类型有: {{ selectValues }}</p>
-    </div>
-    <div>
-      <h1>表单相关修饰符</h1>
-      <h2>v-model.lazy</h2>
-      <input type="text" v-model.lazy="formContent" />
-      <p>输入的内容: {{ formContent }}</p>
-      <p>内容的类型: {{ typeof formContent }}</p>
-      <p>内容的长度: {{ formContent.length }}</p>
-      <h2>v-model.number</h2>
-      <input type="text" v-model.number="formContent2" />
-      <p>输入的内容: {{ formContent2 }}</p>
-      <p>内容的类型: {{ typeof formContent2 }}</p>
-      <p>内容的长度: {{ formContent2.length }}</p>
-      <h2>v-model.trim</h2>
-      <input type="text" v-model.trim="formContent3" />
-      <p>输入的内容: {{ formContent3 }}</p>
-      <p>内容的类型: {{ typeof formContent3 }}</p>
-      <p>内容的长度: {{ formContent3.length }}</p>
+      <div class="footer-right">
+        <div>合计(不含运费): <span style="color: red; font-weight: bold;">￥{{ totalPrice }}</span></div>
+        <button>去结算</button>
+      </div>
     </div>
   </div>
 </template>
@@ -70,66 +49,65 @@
 <script setup>
 import { computed, ref } from 'vue';
 
-const textContent = ref('');
-const isChecked = ref(false);
-const checkedValue = ref('yes');
-const hobby = ref([]);
-const checkedArrData = ref([
-  { id: 'swim', title: '游泳', value: '游个泳' },
-  { id: 'run', title: '跑步', value: '跑个步' },
-  { id: 'ride', title: '骑行', value: '骑个行' },
-  { id: 'jump', title: '跳绳', value: '跳个绳' },
-  { id: 'basketball', title: '打篮球', value: '打个球' }
+const shoppingData = ref([
+  {
+    id: 1,
+    goods_name: '华为手环',
+    goods_price: 169,
+    goods_num: 1,
+    checked: true
+  },
+  {
+    id: 2,
+    goods_name: '索尼耳机',
+    goods_price: 163,
+    goods_num: 1,
+    checked: true
+  },
+  {
+    id: 3,
+    goods_name: '小米汽车',
+    goods_price: 1890,
+    goods_num: 1,
+    checked: false
+  },
+  {
+    id: 4,
+    goods_name: 'iPhone14',
+    goods_price: 1699,
+    goods_num: 1,
+    checked: true
+  }
 ]);
-const message = computed(() => {
-  if (hobby.value.length === 0) return;
-  else return hobby.value.join('、');
+
+// 删除
+const deleteShopping = (id) => {
+  shoppingData.value = shoppingData.value.filter(item => item.id !== id);
+}
+// 删除选中的商品
+const deleteChooseShopping = () => {
+  shoppingData.value = shoppingData.value.filter(item => !item.checked);
+}
+
+// 全选
+const chooseAllShopping = computed({
+  get: () => shoppingData.value.every(item => item.checked),
+  set: (val) => {
+    shoppingData.value.forEach(item => item.checked = val);
+  }
 })
 
-const gender = ref('保密');
-const selectArrData = ref([
-  { key: '射手', value: '百里守约' },
-  { key: '法师', value: '王昭君' },
-  { key: '坦克', value: '程咬金' },
-  { key: '辅助', value: '大乔' },
-  { key: '刺客', value: '兰陵王' },
-  { key: '战士', value: '铠' }
-]);
-const selectValue = ref('');
-const selectValues = ref([]);
-setTimeout(() => {
-  gender.value = '男';
-}, 2000);
-
-// 表单相关修饰符
-const formContent = ref('');
-const formContent2 = ref('');
-const formContent3 = ref('');
+// 总价
+const totalPrice = computed(() => {
+  return shoppingData.value.reduce((total, item) => {
+    if (item.checked) {
+      total += item.goods_price * item.goods_num;
+    }
+    return total;
+  }, 0);
+})
 </script>
 
 <style scoped>
-* {
-  transition: all 1s;
-}
-
-input {
-  padding: 5px 0 5px 5px;
-}
-
-.container {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 20px;
-  position: relative;
-}
-
-.hr {
-  position: absolute;
-  height: 100%;
-  left: 50%;
-  top: -10px;
-  transform: translateX(-50%) rotate(180deg);
-  border-width: 1px;
-  border-style: dashed;
-}
+@import "./assets/shopping-cart.css";
 </style>
