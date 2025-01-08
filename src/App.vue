b8dae8ac3ff5a570b40cd9c84a08da294caef96b
LvYuanWang <1175430934@qq.com>
2025-01-08 15:14:44 +0800

    vue3侦听器2

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,92 @@
 <template>
   <div>
+    <h1>watchEffect</h1>
     <div class="box">
-      <h2>智能机器人</h2>
-      <input type="text" placeholder="请输入问题" v-model.lazy.trim="question" />
-      <div v-if="loading">正在加载中...</div>
-      <div v-else>{{ answer }}</div>
-    </div>
-    <div class="box">
-      <h2>侦听计算属性</h2>
-      <input type="text" v-model.lazy.trim="firstName" />
-      <input type="text" v-model.lazy.trim="lastName" />
-      <div>{{ fullName }}</div>
-    </div>
-    <div class="box">
-      <h2>侦听计算属性</h2>
+      <h2>团队管理</h2>
       <ul>
-        <li v-for="task in tasks.list" :key="task.id">
-          {{ task.title }} - {{ task.completed ? '已完成' : '未完成' }}
-          <button @click="task.completed = !task.completed">切换状态</button>
+        <li v-for="member in team.members" :key="member.id">
+          {{ member.name }} - {{ member.role }} - {{ member.status }}
         </li>
       </ul>
+      <button @click="updateLeaderStatus">切换领导的状态</button>
+      <button @click="updateMemberStatus">切换成员的状态</button>
     </div>
     <div class="box">
-      <h2>侦听Getter函数</h2>
-      <input type="number" v-model.number="count1" />
-      <input type="number" v-model.number="count2" />
-      <div>{{ count1 }} - {{ count2 }}</div>
+      <h2>回调触发的时机</h2>
+      <button @click="isShow = !isShow">显示/隐藏</button>
+      <div v-if="isShow" ref="divRef">
+        <p>this is a test</p>
+      </div>
+      <p>上面的高度为: {{ height }}</p>
+    </div>
+    <div class="box">
+      <h2>停止侦听器</h2>
+      <button @click="num++">{{ num }}</button>
+      <p>{{ message }}</p>
     </div>
   </div>
 </template>
 
 <script setup>
-import { computed, reactive, ref, watch } from 'vue';
+import { reactive, ref, watch, watchEffect } from 'vue';
 
-const question = ref('');
-const loading = ref(false);
-const answer = ref('');
-const options = {
-  method: 'POST',
-  headers: {
-    'Api-Key': 'd5j1si4pap69q3dp',
-    'Api-Secret': 'yxmk8ld5',
-    'Content-Type': 'application/json;charset=UTF-8'
-  }
-};
-
-watch(question, async (newVal) => {
-  if (!newVal) {
-    answer.value = '';
-    return;
-  }
-  loading.value = true;
-  try {
-    const res = await fetch('/reply', {
-      ...options,
-      body: JSON.stringify({
-        content: question.value,
-        type: 1,
-        from: Math.random().toString(36).substr(2)
-      })
-    }).then(res => res.json());
-    answer.value = res.data[0].content;
-  } catch {
-    answer.value = '请求失败';
-  } finally {
-    loading.value = false;
-  }
-});
-
-// 侦听计算属性
-const firstName = ref('John');
-const lastName = ref('Doe');
-const fullName = computed(() => `${firstName.value} ${lastName.value}`);
-watch(fullName, (newValue, oldValue) => {
-  console.log(`newValue: ${newValue}, oldValue: ${oldValue}`);
-});
-
-// 侦听reactive响应式对象
-const tasks = reactive({
-  list: [
-    { id: 1, title: 'Task 1', completed: false },
-    { id: 2, title: 'Task 2', completed: true },
-    { id: 3, title: 'Task 3', completed: false }
+const team = reactive({
+  members: [
+    { id: 1, name: 'Alice', role: 'Leader', status: 'Active' },
+    { id: 2, name: 'Bob', role: 'Member', status: 'Active' }
   ]
 });
-watch(tasks, () => {
-  console.log('tasks 发生了变化');
-});
 
-// 侦听Getter函数
-const count1 = ref(0);
-const count2 = ref(0);
-function isEven() {
-  console.log('isEven 函数执行了');
-  console.log(count2.value);
-  return count1.value % 2 === 0;
+function updateLeaderStatus() {
+  const leader = team.members.find(member => member.role === 'Leader');
+  // 切换状态
+  leader.status = leader.status === 'Active' ? 'Inactive' : 'Active';
 }
-watch(isEven, () => {
-  console.log("触发了 isEven 的侦听器");
-});
-watch([count1, count2], () => {
-  console.log("触发了 count1 或 count2 的侦听器");
+
+function updateMemberStatus() {
+  const member = team.members.find(member => member.role === 'Member');
+  // 切换状态
+  member.status = member.status === 'Active' ? 'Inactive' : 'Active';
+}
+
+watchEffect(() => {
+  const leader = team.members.find(me => me.role === 'Leader');
+  console.log('Leader status:', leader.status);
+})
+
+
+// 回调触发的时机
+const isShow = ref(false);
+const height = ref(0);
+const divRef = ref(null);
+// watch写法
+watch(isShow, () => {
+  height.value = divRef.value ? divRef.value.offsetHeight : 0;
+  console.log(`当前获取的高度为: ${height.value}`);
+}, { flush: 'post' });
+// watchEffect写法
+// watchEffect(() => {
+//   height.value = divRef.value ? divRef.value.offsetHeight : 0;
+//   console.log(`当前获取的高度为: ${height.value}`);
+// });
+
+// 停止侦听器
+const num = ref(0);
+const message = ref('');
+const unwatch = watch(num, (newValue) => {
+  // 当num等于为5时停止侦听
+  if (newValue === 5) {
+    unwatch();
+  }
+  message.value = `当前num值为: ${num.value}`;;
 })
 </script>
 
 <style scoped>
 .box {
   padding: 5px;
+  margin-bottom: 5px;
   border: 1px solid #ccc;
-  margin-bottom: 10px;
+  box-sizing: border-box;
 }
 </style>
